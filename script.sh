ecda728a5b84b15c641b8a9f4299bc6700edb084
Ketan <ketan.designs@gmail.com>
2024-08-13 22:59:10 -0400

    Fixed after running ShellCheck
    - Prefer sub-shell instead of backticks for command substitution.
    - Declare and assign variables separately to avoid masking return values.
    - Double quote to prevent globbing and work splitting.
    - Use `${variable//find/replace` instead of `echo | sed`.
    - Remove necessary `echo` when assigning variables.
    - Remove unneessary `$` within arithmetic expression.

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@
 set -euo pipefail
 
 # Start
-start_time=`date`
+start_time=$(date)
 echo "Started at: $start_time"
 
 PATH_TO_ENTRIES="entries"
@@ -34,12 +34,19 @@ fi
 #   Creates Markdown file per entry.
 #######################################
 function generate_markdown() {
-  local entry="$PATH_TO_ENTRIES/$1"
+  local entry
+  local extracted_created_date
+  local extracted_entry_text
+  local has_tags
+  local entry_md5
+  local markdown 
+  
+  entry="$PATH_TO_ENTRIES/$1"
 
-  local extracted_created_date=$(plutil -extract "Creation Date" raw $entry)
-  local extracted_entry_text=$(plutil -extract "Entry Text" raw $entry)
+  extracted_created_date=$(plutil -extract "Creation Date" raw "$entry")
+  extracted_entry_text=$(plutil -extract "Entry Text" raw "$entry")
 
-  local has_tags=false
+  has_tags=false
   if plutil -extract "Tags" xml1 -o /dev/null "$entry" &>/dev/null; then
     has_tags=true
   fi
@@ -47,28 +54,30 @@ function generate_markdown() {
   # HAX: Is there a better way to include newlines when appending a string with a value?
   local extracted_tags="tags:"
   if [[ "$has_tags" == true ]]; then
-    local entry_tag_count=$(plutil -extract "Tags" raw $entry)
-    if [[ $entry_tag_count > 0 ]]; then
+    local entry_tag_count
+    entry_tag_count=$(plutil -extract "Tags" raw "$entry")
+    if [[ $entry_tag_count -gt 0 ]]; then
       for ((tag_index = 0; tag_index < entry_tag_count; tag_index++)); do
-        local extracted_tag=$(plutil -extract "Tags".$tag_index raw $entry | tr ' ' '-')
+        local extracted_tag
+        extracted_tag=$(plutil -extract "Tags".$tag_index raw "$entry" | tr ' ' '-')
         extracted_tags="$extracted_tags
   - $extracted_tag"
       done
     fi
   fi
 
-  local entry_md5=$(md5 -q $entry)
-  output_file_name=$(echo $extracted_created_date | sed 's/:/-/g')
+  entry_md5=$(md5 -q "$entry")
+  output_file_name="${extracted_created_date//:/-}"
   output_file="$PATH_TO_EXPORT/$output_file_name-$entry_md5.md"
 
-  local markdown="---
+  markdown="---
 date: $extracted_created_date
 $extracted_tags
 ---
 
 $extracted_entry_text"
 
-  echo "$markdown" > $output_file
+  echo "$markdown" > "$output_file"
 
   echo "✓ $entry exported to $output_file"
 }
@@ -84,8 +93,11 @@ $extracted_entry_text"
 #   Prints statistics to console.
 #######################################
 function summary() {
-  local entries_count=$(ls $PATH_TO_ENTRIES | wc -l)
-  local export_count=$(ls $PATH_TO_EXPORT | wc -l)
+  local entries_count
+  local export_count
+
+  entries_count=$(ls $PATH_TO_ENTRIES | wc -l)
+  export_count=$(ls $PATH_TO_EXPORT | wc -l)
   echo "SUMMARY"
   echo "------------------------"
   echo "Entries: $entries_count"
@@ -93,17 +105,17 @@ function summary() {
 }
 
 for entry in ${ENTRIES[@]}; do
-  generate_markdown $entry
+  generate_markdown "$entry"
 done
 
 summary
 
 # End
-end_time=`date`
+end_time=$(date)
 echo "Completed at: $end_time"
 
 start_time=$(date -j -f "%a %b %d %H:%M:%S %Z %Y" "$start_time" "+%s")
 end_time=$(date -j -f "%a %b %d %H:%M:%S %Z %Y" "$end_time" "+%s")
 
-DURATION=$(($end_time - $start_time))
+DURATION=$((end_time - start_time))
 echo "Duration: $DURATION seconds"
